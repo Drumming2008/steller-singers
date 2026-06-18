328e1540ab9c2fc1c0c39e282817381daefe20a3
Drumming2008 <finn.reese@gmail.com>
2026-06-17 20:48:29 -0700

    big readme update and other changes

--- readme.md
+++ readme.md
@@ -1,1 +1,27 @@
 # Steller Singers website!
+
+To edit anything here, the Steller google account has access to this repo. Just sign in to Github using the "Sign in with Google" button.
+
+## Editing bios
+
+Find the `data` folder and open `bios.txt`. The format should be something like this:
+```
+Name 1
+image.png
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+===
+Name 2
+image_2.png
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+```
+
+All the data is split by line (do not add any newlines in your bios, just trust that the text will wrap on the website). The data should be input as follows:
+
+1. The first line is the name.
+2. The second line is the name of the image file of the singer. This image needs to be put in the folder: `data/member_images`. Make sure to include the file extension (such as `.png`, `.jpg`, etc.).
+3. The third line is the bio. If you want more styling or would like to add things like links, you can use markdown. [Here's a handy guide if you've never used it before.](https://www.markdownguide.org/cheat-sheet/)
+4. Between each entry put "===" as a divider.
+
+## Updating logos
+
+To update any logos used go to the `logos` folder and replace any of the images within it with a file that has the same name and extension.
--- script.js
+++ script.js
@@ -1,3 +1,81 @@
+// make hamburger menu open/close
+
 document.getElementById("hamburger-button").onclick = () => {
   document.getElementById("hamburger").classList.toggle("open")
 }
+
+// create links to each section
+for (let i of [...document.querySelectorAll("h2")].reverse()) {
+  let li = document.createElement("li")
+  li.innerHTML = `
+    <a href="#${i.parentElement.id}">${i.innerText}</a>
+  `
+  document.querySelector("#hamburger > ul").prepend(li)
+}
+
+// make hamburger links animate fancily
+for (let i of document.querySelectorAll("#hamburger a")) {
+  let newLink = i.cloneNode(true)
+  newLink.classList.add("hamburger-link-overlay")
+  newLink.setAttribute("aria-hidden", true)
+  newLink.style.pointerEvents = "none"
+  newLink.tabIndex = "-1"
+  i.after(newLink)
+
+  i.onmouseenter = i.onfocus = () => {
+    i.style.setProperty("--rotation-modifier", [-1, 1][Math.floor(Math.random() * 2)])
+  }
+}
+
+// custom element for steller logo
+class StellerIcon extends HTMLElement {
+  static get observedAttributes() {
+    return ["src"]
+  }
+
+  constructor() {
+    super()
+  }
+
+  connectedCallback() {
+    this.updateMask()
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name == "src" && oldValue !== newValue) {
+      this.updateMask()
+    }
+  }
+
+  updateMask() {
+    let src = this.getAttribute("src")
+    if (!src) return
+    this.style.maskImage = `url("${src}")`
+    this.style.webkitMaskImage = `url("${src}")`
+  }
+}
+
+customElements.define("steller-icon", StellerIcon)
+
+// create bios
+fetch("./data/bios.txt").then(r => {
+  if (r.ok) return r.text()
+}).then(text => {
+  let bios = text.split("===\n")
+  for (let i of bios) {
+    let data = i.split("\n")
+    /*
+      0: name
+      1: image
+      2: bio
+    */
+    let bioWrapper = document.createElement("div")
+    bioWrapper.classList.add("bio-wrapper")
+    bioWrapper.innerHTML = `
+      <h3>${data[0]}</h3>
+      <img src="./data/member_images/${data[1]}">
+      <p>${data[2]}</p>
+    `
+    document.getElementById("members").append(bioWrapper)
+  }
+})
